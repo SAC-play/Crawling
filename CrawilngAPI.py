54a49d2067557c50c9afd7a0840d079866b58be8
khw <hyunwook711@naver.com>
2021-05-21 19:39:39 +0900

    currency_list add

--- CrawilngAPI.py
+++ CrawilngAPI.py
@@ -1,1 +1,7 @@
-print("Hello")
+def crawling(accountList):
+    currencyList =[]
+    for a in accountList :
+        if(a['currency'] == 'KRW'):
+            continue
+        currencyList.append(a['currency'])
+    print(currencyList)
